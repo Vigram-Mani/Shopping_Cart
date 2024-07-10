e87156cae94a633c8f7edfad993fa7b1e0dd80ec
VIGRAM K M <vigramkm@gmail.com>
2024-07-10 16:49:31 +0530

    Initial Commit

---
+++ lib/screens/cart/cart_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../providers/cart_provider.dart';
+
+class CartScreen extends ConsumerStatefulWidget {
+  const CartScreen({super.key});
+
+  @override
+  ConsumerState<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends ConsumerState<CartScreen> {
+  bool showCoupon = true;
+
+  @override
+  Widget build(BuildContext context) {
+    final cartProducts = ref.watch(cartNotifierProvider);
+    final total = ref.watch(cartTotalProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Cart'),
+        centerTitle: true,
+        // actions: [],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Column(
+              children: cartProducts.map((product) {
+                return Container(
+                  padding: const EdgeInsets.only(top: 10, bottom: 10),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        product.image,
+                        width: 60,
+                        height: 60,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text('${product.title}...'),
+                      const Expanded(child: SizedBox()),
+                      Text('💲${product.price}')
+                    ],
+                  ),
+                );
+              }).toList(), // output cart products he
+            ),
+            Text("Total Price = 💲$total"),
+            // output totals here
+          ],
+        ),
+      ),
+    );
+  }
+}
